ce7e460e1f41c117b38e8ed4b31f41bd9e4a1ddb
Khoa <khoauranium@gmail.com>
2025-11-15 17:36:18 +0700

    Refund

---
+++ src/WorkSpace.Application/DTOs/Refund/CreateRefundRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkSpace.Application.DTOs.Refund
+{
+    public class CreateRefundRequestDto
+    {
+        [Required]
+        [MaxLength(1000)]
+        public string Notes { get; set; }
+    }
+}
