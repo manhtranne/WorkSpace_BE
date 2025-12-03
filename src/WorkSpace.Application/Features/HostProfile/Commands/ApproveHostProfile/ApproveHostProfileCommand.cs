a7cbefa614b2493f3b1a2b2d4d6c70fedf89f037
Khoa <khoauranium@gmail.com>
2025-12-03 20:33:57 +0700

    .

--- src/WorkSpace.Application/Features/HostProfile/Commands/ApproveHostProfile/ApproveHostProfileCommand.cs
+++ src/WorkSpace.Application/Features/HostProfile/Commands/ApproveHostProfile/ApproveHostProfileCommand.cs
@@ -1,68 +1,69 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
-using System.Linq; 
+using System.Linq;
 using WorkSpace.Application.Enums;
 using WorkSpace.Application.Interfaces.Repositories;
 using WorkSpace.Application.Wrappers;
 using WorkSpace.Domain.Entities;
 
-namespace WorkSpace.Application.Features.HostProfile.Commands.ApproveHostProfile;
-
-public class ApproveHostProfileCommand : IRequest<Response<bool>>
+namespace WorkSpace.Application.Features.HostProfile.Commands.ApproveHostProfile
 {
-    public int HostProfileId { get; set; }
-    public bool IsApproved { get; set; }
-}
-
-public class ApproveHostProfileHandler : IRequestHandler<ApproveHostProfileCommand, Response<bool>>
-{
-    private readonly IHostProfileAsyncRepository _hostProfileRepository;
-    private readonly UserManager<AppUser> _userManager;
-
-    public ApproveHostProfileHandler(
-        IHostProfileAsyncRepository hostProfileRepository,
-        UserManager<AppUser> userManager)
+    public class ApproveHostProfileCommand : IRequest<Response<bool>>
     {
-        _hostProfileRepository = hostProfileRepository;
-        _userManager = userManager;
+        public int HostProfileId { get; set; }
+        public bool IsApproved { get; set; }
     }
 
-    public async Task<Response<bool>> Handle(ApproveHostProfileCommand request, CancellationToken cancellationToken)
+    public class ApproveHostProfileHandler : IRequestHandler<ApproveHostProfileCommand, Response<bool>>
     {
-        var hostProfile = await _hostProfileRepository.GetByIdAsync(request.HostProfileId, cancellationToken);
-        if (hostProfile == null)
+        private readonly IHostProfileAsyncRepository _hostProfileRepository;
+        private readonly UserManager<AppUser> _userManager;
+
+        public ApproveHostProfileHandler(
+            IHostProfileAsyncRepository hostProfileRepository,
+            UserManager<AppUser> userManager)
         {
-            return new Response<bool>("Không tìm thấy hồ sơ Host.");
+            _hostProfileRepository = hostProfileRepository;
+            _userManager = userManager;
         }
 
-        hostProfile.IsVerified = request.IsApproved;
-        hostProfile.LastModifiedUtc = DateTime.UtcNow;
-
-        await _hostProfileRepository.UpdateAsync(hostProfile, cancellationToken);
-
-
-        if (request.IsApproved)
+        public async Task<Response<bool>> Handle(ApproveHostProfileCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userManager.FindByIdAsync(hostProfile.UserId.ToString());
-            if (user != null)
+            var hostProfile = await _hostProfileRepository.GetByIdAsync(request.HostProfileId, cancellationToken);
+            if (hostProfile == null)
             {
-                var currentRoles = await _userManager.GetRolesAsync(user);
-
-              
-                if (currentRoles.Any())
-                {
-                    await _userManager.RemoveFromRolesAsync(user, currentRoles);
-                }
-
-              
-                await _userManager.AddToRoleAsync(user, nameof(Roles.Owner));
+                return new Response<bool>("Không tìm thấy hồ sơ Host.");
             }
+
+            hostProfile.IsVerified = request.IsApproved;
+            hostProfile.LastModifiedUtc = DateTime.UtcNow;
+
+            await _hostProfileRepository.UpdateAsync(hostProfile, cancellationToken);
+
+            if (request.IsApproved)
+            {
+                var user = await _userManager.FindByIdAsync(hostProfile.UserId.ToString());
+                if (user != null)
+                {
+                   
+                    var currentRoles = await _userManager.GetRolesAsync(user);
+
+   
+                    if (currentRoles.Any())
+                    {
+                        await _userManager.RemoveFromRolesAsync(user, currentRoles);
+                    }
+
+                   
+                    await _userManager.AddToRoleAsync(user, nameof(Roles.Owner));
+                }
+            }
+
+            var message = request.IsApproved
+                ? "Đã duyệt hồ sơ. Tài khoản đã chuyển sang quyền Owner."
+                : "Đã từ chối hồ sơ Host.";
+
+            return new Response<bool>(true, message);
         }
-
-        var message = request.IsApproved
-            ? "Đã duyệt hồ sơ và chuyển tài khoản sang quyền Owner."
-            : "Đã từ chối hồ sơ Host.";
-
-        return new Response<bool>(true, message);
     }
 }
