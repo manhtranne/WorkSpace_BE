9705af1c45f5929fd0cb152457f6ea4c8feb3d9e
Võ Trần Đăng Khoa <khoauranium@gmail.com>
2025-12-16 14:20:58 +0700

    Get All Drink Service

---
+++ src/WorkSpace.Application/DTOs/Services/WorkSpaceWithServicesDto.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace WorkSpace.Application.DTOs.Services
+{
+    public class WorkSpaceWithServicesDto
+    {
+        public int WorkSpaceId { get; set; }
+        public string WorkSpaceTitle { get; set; } = default!;
+        public List<WorkSpaceServiceDto> Services { get; set; } = new();
+    }
+}
---
+++ src/WorkSpace.Application/Features/Services/Queries/GetAllServicesGrouped/GetAllServicesGroupedQuery.cs
@@ -0,0 +1,13 @@
+﻿
+using MediatR;
+using WorkSpace.Application.DTOs.Services;
+using WorkSpace.Application.Wrappers;
+using System.Collections.Generic;
+
+namespace WorkSpace.Application.Features.Services.Queries.GetAllServicesGrouped
+{
+    public class GetAllServicesGroupedQuery : IRequest<Response<List<WorkSpaceWithServicesDto>>>
+    {
+        public int OwnerUserId { get; set; }
+    }
+}
