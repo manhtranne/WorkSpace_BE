be8b515f0b4093e46ae7bb6f58ce26c1e5d65fba
Võ Trần Đăng Khoa <khoauranium@gmail.com>
2025-12-20 15:11:36 +0700

    Get All Review In WorkSpace

---
+++ src/WorkSpace.Application/DTOs/Reviews/WorkSpaceReviewDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WorkSpace.Application.DTOs.Reviews
+{
+    public class WorkSpaceReviewDto
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public int UserId { get; set; }
+        public string ReviewerName { get; set; }
+        public string? ReviewerAvatar { get; set; }
+
+        public int RoomId { get; set; }
+        public string RoomName { get; set; } 
+        public string RoomType { get; set; }
+    }
+}
